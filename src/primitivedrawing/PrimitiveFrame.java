7899a4df8e412a5dbd808b0e3265b2406beb0c42
Marko Rankovic <markocrush96@gmail.com>
2019-11-05 23:10:03 +0000

    program exits when any of the windows close

---
+++ src/primitivedrawing/PrimitiveFrame.java
@@ -0,0 +1,14 @@
+package primitivedrawing;
+
+import javax.swing.JFrame;
+
+public class PrimitiveFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public PrimitiveFrame() {
+		super();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Program exits on close of any frame.
+	}
+	
+}
